3c918236254e9e7386f100e56864ade7aae8599e
Sushil Sharma <sushilsharma3jan1998@gmail.com>
2021-12-15 00:48:29 +0530

    Finding Peak element in an Array

---
+++ PeakElement.java
@@ -0,0 +1,19 @@
+package com.sushil.DSA;
+
+public class PeakElement {
+
+	static int findPeakElement(int arr[])
+	{
+		int max = arr[0];
+		for(int i = 0; i<arr.length; i++)
+		{
+			if(arr[i] > max)
+				max = arr[i];
+		}
+		return max;
+	}
+	public static void main(String[] args) {
+		int arr[] = {1,54,67,34,390};
+		System.out.println("Peak Element: " + findPeakElement(arr));
+	}
+}
