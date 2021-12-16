e56dfadbf16f2cf8568382780a731a88b8ea063d
Sushil Sharma <sushilsharma3jan1998@gmail.com>
2021-12-17 00:53:50 +0530

    Printing star patterns using nested Loops

--- PatternProblemsSimple.java
+++ PatternProblemsSimple.java
@@ -54,6 +54,23 @@ public class PatternProblemsSimple {
 			System.out.println();
 		}
 		
-	
+
+//		 	 * 
+//		    * * 
+//		   * * * 
+//		  * * * * 
+//		 * * * * * 
+		
+		
+		for(int i=1;i<=5;i++) {
+		   for(int j=5;j>=i;j--) {
+		    System.out.print("-");
+		   }
+		   for(int j=1;j<=i;j++) {
+		    System.out.print("* ");
+		   }
+		   
+		   System.out.println();
+		}
 	}
 }
