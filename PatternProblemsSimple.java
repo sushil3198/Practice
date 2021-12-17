b79cc62471fc715a23e52f400c20f1579be5e947
Sushil Sharma <sushilsharma3jan1998@gmail.com>
2021-12-18 00:02:59 +0530

    Updated Star Pattern file with new COmplex Patterns

--- PatternProblemsSimple.java
+++ PatternProblemsSimple.java
@@ -76,6 +76,16 @@ public class PatternProblemsSimple {
 		}
 		System.out.println();
 		
+		
+//		for n = 5
+		
+		//        *  
+		//      *  *  
+		//    *  *  *  
+		//  *  *  *  *  
+		//*  *  *  *  *  
+
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter some input number: ");
 		int n = sc.nextInt();
@@ -88,6 +98,71 @@ public class PatternProblemsSimple {
 			for(int j = 1; j<=i; j++)
 			{
 				System.out.print("*  ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		//        1  
+		//      2  3  
+		//    4  5  6  
+		//  7  8  9  10  
+		//11  12  13  14  15  
+
+		
+		
+		Scanner sc1 = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int number = 1;
+		for(int i = 1; i<=n; i++)
+		{
+			for(int j = 1; j<= n-i; j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j = 1; j<= i ; j++)
+			{
+				System.out.print(number++ + "  ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		//		* 
+		//		* * 
+		//		* * * 
+		//		* * * * 
+		//		* * * 
+		//		* * 
+		//		* 
+
+		
+		Scanner sc2 = new Scanner(System.in);
+
+		
+		int n3 = sc.nextInt();
+		
+		
+		int rows = 2*n3-1;
+		
+		for(int i = 1; i<=rows; i++)
+		{
+			if(i<=n3)
+			{
+				for(int j=1; j<=i; j++)
+				{
+					System.out.print("* ");
+				}	
+			}
+			else {
+				for(int j = 1; j <= rows-i+1; j++)
+				{
+					System.out.print("* ");
+				}
 			}
 			System.out.println();
 		}
