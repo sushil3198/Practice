9ef2738a0872cead8d7d15dc0b1468cc6cc17cdc
Sushil Sharma <sushilsharma3jan1998@gmail.com>
2022-02-10 21:19:18 +0530

    Wrapper LWC updated

--- LWC/LWC_Components/wrapperLWC/wrapperLWC.js
+++ LWC/LWC_Components/wrapperLWC/wrapperLWC.js
@@ -6,33 +6,43 @@ export default class WrapperLWC extends LightningElement {
     receivedData;
     contactList;
     oppsList;
+    genericObject = {};
+    mergedObj;
+
     inputHandler(event) {
         this.searchString = event.target.value;
         console.log('searchTerm: ', this.searchString);
     }
 
-
     searchHandler() {
-
         returnList({ inputName: this.searchString }).then(response => {
             this.receivedData = response;
             console.log('Response: ', this.receivedData);
-            for(let key in this.receivedData){
-                console.log(key, this.receivedData[key]);
-                //this.contactList = this.receivedData[key];
-                //console.log('contactList: ', this.contactList);
-            }
             this.contactList = this.receivedData.cList;
             this.oppsList = this.receivedData.oppsList;
-            //let temp = JSON.stringify(this.receivedData);
-           // this.contactList
             console.log('Contacts: ', this.contactList);
             console.log('Opportunity: ', this.oppsList);
+            for(let key in this.contactList){
+                this.genericObject[key] = {name : this.contactList[key].LastName}
+            }
 
+            for(let key in this.oppsList){
+                this.genericObject[key] = {name : this.oppsList[key].Name, recordId : this.oppsList[key].Id}
+            }
+            console.log('generic',this.genericObject);
+
+            this.mergedObj = this.oppsList.concat(this.contactList);
+            console.log(this.mergedObj);
+
+           // Object.
         }).catch(error => {
             console.log('Error: ', error.body.message);
             console.log('Error Obj: ', error);
         })
     }
+
+    
+
+    
 
 }
